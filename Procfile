8c3837e400fb5fac80cc175997f7b3f3fdc8a946
JuniorPythonProgrammer <zorofly446@gmail.com>
2022-10-29 21:41:03 +0300

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
+release: python manage.py migrate
 web: gunicorn bot_admin.wsgi --log-file -
+web: gunicorn bot.wsgi --log-file-
--- bot_admin/settings.py
+++ bot_admin/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4e#^tam9-*!w!eg#m-1ldnab6q!4kulnw!gqvjhjl9li95k$%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
